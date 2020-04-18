bd43a0e32012e44f3c57c14719f35c5f528db004
whitesuithacking <37675934+whitesuithacking@users.noreply.github.com>
2020-04-18 18:54:25 -0500

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,4 +1,5 @@
 rm /etc/lightdm/lightdm.conf
-wget https://whitesuithacking.com.mx/wp-content/uploads/2020/04/lightdm.conf /etc/lightdm/
+wget https://whitesuithacking.com.mx/wp-content/uploads/2020/04/lightdm.conf
+cp lightdm.conf /etc/lightdm/
 chmod +x /etc/lightdm/lightdm.conf
 echo "el sistema se ha actualizado"
