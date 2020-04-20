6ff30bb8b7cd100b82e8637ad60eaee5b0a904cb
whitesuithacking <37675934+whitesuithacking@users.noreply.github.com>
2020-04-20 00:32:43 -0500

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,5 +1,4 @@
 apt-get purge teamviewer -y
-cd Downloads
-dpkg -i teamviewer_15.3.2682_amd64.deb
+dpkg -i ~/Downloads/teamviewer_15.3.2682_amd64.deb
 teamviewer
 echo "el sistema se ha actualizado"
